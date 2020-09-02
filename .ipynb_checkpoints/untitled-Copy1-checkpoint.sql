4f1a00deb706443e946b4a2b98883e3d7befc313
david griffiths <davidgg777@hotmail.com>
2020-09-03 09:11:01 +1000

    cleaned up track files

--- .ipynb_checkpoints/untitled-Copy1-checkpoint.sql
+++
@@ -1,7 +0,0 @@
-select "Case Snap Date", count(*)
-FROM "enc_dde"."tableau enc case field dave view" "A61S Data Quality Indicators"
-  INNER JOIN "enc_dde"."tableau dave view" "A61S Case" ON ("A61S Data Quality Indicators"."case_key" = "A61S Case"."case_key")
-  INNER JOIN "enc_dde"."tableau dave" "A61S Case Snapshot" ON ("A61S Data Quality Indicators"."case_key" = "A61S Case Snapshot"."case_key")
-  INNER JOIN "nbn_dde_23"."tableau dave loc view" "A61S Location" ON ("A61S Data Quality Indicators"."location_key" = "A61S Location"."location key")
-  where "Case Snap Date" > current_date-4
-  group by 1
--- untitled.sql
+++
@@ -1,7 +0,0 @@
-select "Case Snap Date", count(*)
-FROM "enc_dde"."tableau enc case field links view" "A61S Data Quality Indicators"
-  INNER JOIN "enc_dde"."tableau enc case view" "A61S Case" ON ("A61S Data Quality Indicators"."case_key" = "A61S Case"."case_key")
-  INNER JOIN "enc_dde"."tableau enc case snapshot view" "A61S Case Snapshot" ON ("A61S Data Quality Indicators"."case_key" = "A61S Case Snapshot"."case_key")
-  INNER JOIN "nbn_dde_23"."tableau location view" "A61S Location" ON ("A61S Data Quality Indicators"."location_key" = "A61S Location"."location key")
-  where "Case Snap Date" > current_date-4
-  group by 1
